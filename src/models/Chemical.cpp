ab35021026a0ba1e77f62dc88128457e8678666a
metalicn20 <metalicn20@gmail.com>
2020-08-14 01:30:28 +0430

    Add lpsolve as a dynamic library

--- src/models/Chemical.cpp
+++ src/models/Chemical.cpp
@@ -23,24 +23,38 @@ void Chemical::setPercentage(double value)
 {
     string baseElement = compositions->getBaseElement();
     if (getSymbol() == baseElement)
+    {
         return;
+    }
     if (value < 0 || value > 100)
+    {
         throw out_of_range("value");
+    }
     Chemical *baseComposition;
     double sum = value;
     for (size_t i = 0; i < compositions->size(); i++)
     {
         Chemical &cmp = compositions->get(i);
         if (cmp.getSymbol() != baseElement)
+        {
             sum += cmp.getPercentage();
+        }
         else
+        {
             baseComposition = &cmp;
+        }
     }
     if (baseComposition)
+    {
         if (sum + value > 100)
+        {
             throw out_of_range("value");
+        }
         else
+        {
             baseComposition->percentage = 100 - sum;
+        }
+    }
     percentage = value;
 }
 
